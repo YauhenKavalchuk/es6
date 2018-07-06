393d97a78893bb5e8fd2a35673dcd8bfe068dc6d
Yauhen <silent_control@tut.by>
2018-07-06 09:27:31 +0300

    Lesson 2 (let, const, var)

--- app/js/common.js
+++ app/js/common.js
@@ -1,2 +1,56 @@
-let a = 5;
-const B = 6;
+// let, const, var
+// ---------------
+
+// Define
+// let - var
+// const - constant
+// var - deprecated
+
+// Scope
+/*
+if(true) {
+  const a = 'ES5';
+}
+console.log(a);
+*/
+
+// Hoisting
+/*
+var b = undefind;
+console.log(b);
+b = 5;
+*/
+// Example
+/*
+var buttons = document.querySelectorAll('button');
+for (var i = 0; i < buttons.length; i++) {
+  var button = buttons[i];
+  button.innerText = i;
+  button.onclick = function(x) {
+    return function() {
+      console.log(x);
+    };
+  }(i);
+};
+*/
+/*
+var buttons = document.querySelectorAll('button');
+for (let i = 0; i < buttons.length; i++) {
+  var button = buttons[i];
+  button.innerText = i;
+  button.onclick = function() {
+    console.log(i);
+  };
+};
+*/
+// const
+const ARR = [1,2,3,4,5];
+ARR.push(6);
+console.log(ARR);
+
+const OBJ = {
+  es: 'ES6'
+};
+OBJ.es = 'ES7)))';
+OBJ = 'str';
+console.log(OBJ);
