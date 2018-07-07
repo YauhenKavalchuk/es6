3722ae4f240f145d6a2ec946b9af58bd387b7b44
Yauhen <silent_control@tut.by>
2018-07-07 18:27:51 +0300

    Lesson 5 (Arrow function)

--- app/js/common.js
+++ app/js/common.js
@@ -1,63 +1,66 @@
-// Template string
+// Arrow functions
 // ---------------
 
-// Syntax
-// `${dynamic_value}`;
+// Example 1 - default arguments
+/*
+function greet(name = 'Jack', age = 20) {
+  return `Hello ${name}, your age is ${age}!`;
+};
+console.log(greet());
+*/
+// Example 2 - arrow functions syntax
+/*
+// () => {}
+const greet = (name, age) => `Hello ${name}, your age is ${age}!`
+*/
+// Example 3 - cases of syntax
+/*
+// 1) Using name:
+   const FUNC = () => {};
 
-// Example 1 - string
-/*
-function greet(name) {
-  //console.log('Hello ' + name + '!');
-  console.log(`Hello ${name}!`);
-}
-greet('Max');
-*/
-// Example 2 - URL API string
-/*
-const URL = '/api';
-let path1 = 'test';
-let path2 = 'test2';
-let path3 = 'test3';
+// 2) Default syntax:
+   (a,b) => {
+    let sum = a + b;
+    return sum;
+   };
 
-//const API_URL = URL + '/' + path1 + '/' + path2 + '/' + path3;
-const API_URL = `${URL}/${path1}/${path2}/${path3}`;
-console.log(API_URL);
+// 3) One argument:
+   a => {
+    let sum = a + 1;
+    return sum;
+   };
+
+// 4) No arguments:
+   () => {
+    let sum = a + b;
+    return sum;
+   };
+
+// 5) If one line, don't need `return`:
+   a => a * 2;
+   (a,b) => a*b;
+
+// 6) Object literal:
+   () => ({ a:2 });
+
+// 7) IIFE:
+   (() => {null})();
 */
-// Example 3 - Multiple string
-/*
-function emailMessage(name, mail, status) {
-  return `
-    Hello ${name},
-    Your email ${mail} has been ${status}!
-    Thank you for subscription!
-  `
-}
-console.log(emailMessage('Jack', 'jack@mail.com', 'approved'));
-*/
-// Example 4 - Template
-/*
-generate() {
-    return `
-    <li class="news">
-      <a href="${this.url}" target="_blank" class="news__link">
-        <figure class="news__figure">
-          <img src="${this.urlToImage}" alt="${this.title}" class="news__image">
-          <figcaption class="news__description">
-            <h1 class="news__title">${this.title}</h1>
-            <p class="news__text">${this.description}</p>
-            <span class="news__date">${new Date(this.publishedAt).toLocaleDateString()}</span>
-            <cite class="news__author">${this.author}</cite>
-          </figcaption>
-        </figure>
-      </a>
-    </li>`;
+// Example 3 - context
+
+let person = {
+  userName: 'Jack',
+  greet: function() {
+    window.setTimeout(() => {
+      console.log(this.userName, this);
+    }, 1000);
   }
-*/
-// Example 5 - Tagging
+};
+person.greet();
 
-const name = 'Jack';
-console.log(toUppercase`Hello ${name}!`);
-function toUppercase(litArr, value) {
-  console.log(litArr, value);
-  return litArr[0] + value.toUpperCase() + litArr[1];
-}
+
+
+
+ //dsvdsv
+
+ //sdvdsvds
