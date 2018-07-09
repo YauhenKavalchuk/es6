4a86581460a3a61173e5f5a84751eded2c8623cf
Yauhen <silent_control@tut.by>
2018-07-09 09:39:38 +0300

    Lesson 6 (for...of)

--- app/js/common.js
+++ app/js/common.js
@@ -1,66 +1,18 @@
-// Arrow functions
+// for...of
 // ---------------
 
-// Example 1 - default arguments
-/*
-function greet(name = 'Jack', age = 20) {
-  return `Hello ${name}, your age is ${age}!`;
-};
-console.log(greet());
-*/
-// Example 2 - arrow functions syntax
-/*
-// () => {}
-const greet = (name, age) => `Hello ${name}, your age is ${age}!`
-*/
-// Example 3 - cases of syntax
-/*
-// 1) Using name:
-   const FUNC = () => {};
+const names = ['Jack', 'Max', 'Leo'];
 
-// 2) Default syntax:
-   (a,b) => {
-    let sum = a + b;
-    return sum;
-   };
-
-// 3) One argument:
-   a => {
-    let sum = a + 1;
-    return sum;
-   };
-
-// 4) No arguments:
-   () => {
-    let sum = a + b;
-    return sum;
-   };
-
-// 5) If one line, don't need `return`:
-   a => a * 2;
-   (a,b) => a*b;
-
-// 6) Object literal:
-   () => ({ a:2 });
-
-// 7) IIFE:
-   (() => {null})();
-*/
-// Example 3 - context
-
-let person = {
-  userName: 'Jack',
-  greet: function() {
-    window.setTimeout(() => {
-      console.log(this.userName, this);
-    }, 1000);
-  }
-};
-person.greet();
+for(let name in names) {
+  //console.log(name);
+}
 
 
+for(let index in names) {
+  //console.log(names[index]);
+}
 
 
- //dsvdsv
-
- //sdvdsvds
+for(let name of names) {
+  console.log(name);
+}
