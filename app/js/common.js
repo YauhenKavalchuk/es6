60983c448a8ab2a90a424c2fecbb116f64f9a65f
Yauhen <silent_control@tut.by>
2018-07-09 15:53:52 +0300

    Lesson 9 (Inheritance)

--- app/js/common.js
+++ app/js/common.js
@@ -1,28 +1,47 @@
-// Class
+// Inheritance
 // ---------------
 
 class Task {
-  constructor(title = Task.getDefaultTitle(), isCompleted = false) {
+  constructor(title = '') {
     this.title = title,
-    this._isCompleted = isCompleted,
-    Task.counter += 1
-  }
-
-  static getDefaultTitle() {
-    return 'Title text';
+    this.isCompleted = false
   }
 
   completed() {
     this.isCompleted = true;
   }
+}
 
-  get isCompleted() {
-    return this._isCompleted === true ? 'task is completed' : 'task is not completed'
+class SubTask extends Task {
+  constructor(title) {
+    super(title)
+  }
+
+  completed() {
+    super.completed();
+    console.log(`task ${this.title} is completed!`)
   }
 }
 
-Task.counter = 0;
+let task = new Task('Learn ReactJS');
+let subTask = new SubTask('Learn ES6');
 
-let task = new Task('Learn ES6', false);
+task.completed();
+subTask.completed();
 
-console.log(task.isCompleted, task._isCompleted);
+console.log(task);
+console.log(subTask);
+
+//class declaration
+class Task {};
+
+//class expression
+let Task = class Task {};
+
+
+
+
+
+
+
+//
