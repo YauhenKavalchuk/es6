624f8dbbbcde1147af63652482aade36768b719b
Yauhen <silent_control@tut.by>
2018-07-10 09:36:55 +0300

    Lesson 11 (Object destructuring)

--- app/js/common.js
+++ app/js/common.js
@@ -1,26 +1,23 @@
-// Array destructuring
+// Object destructuring
 // ---------------
 
-//const people = ['Jack', 'Max', 'Leo'];
-
-// let jack = people[0];
-// let max = people[1];
-// let leo = people[2];
-
-// let [jack, max, leo] = ['Jack', 'Max', 'Leo']];
-
-// console.log(jack, max, leo);
-
-// const rate = [1,50,100, [1000, 2000]];
-
-// let [low, middle, high, [higher, sup]] = rate;
-
-// console.log(low, middle, high, higher, sup);
-
-function calculate([a,b]) {
-  return [a,b];
+let person = {
+  name: 'Jack',
+  age: 20,
+  address: {
+    city: 'Minsk'
+  }
 }
 
-let [a, b] = calculate([1, 10]);
+//let {name = 'Max',age = 20} = {};
+//let { name: personName, age: personAge } = person;
+//let { address: { city = 'Brest' } } = person;
+//console.log(city);
 
-console.log(a,b);
+function getData({ name, age }) {
+  //console.log(name, age);
+  return { name, age }
+}
+
+let {name, age} = getData(person);
+console.log(name, age);
