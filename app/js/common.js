2c7bef0e70c146d012a6bbf9d905044760d66bee
Yauhen <silent_control@tut.by>
2018-07-12 09:51:58 +0300

    Lesson 13 (Promise)

--- app/js/common.js
+++ app/js/common.js
@@ -1,17 +1,25 @@
-// Symbols
+// Promise
 // ---------------
 
-// let symbol = Symbol.for('mySymbol');
-// let symbolName = Symbol.keyFor(symbol)
-// console.log(symbolName);
+// const promise = new Promise((resolve, reject) => {
+//   throw new Error('some error...');
+//   setTimeout(() => {
+//     if(true) {
+//       resolve('promise completed!');
+//     } else {
+//       reject();
+//     }
+//   }, 1000);
+// });
+//
+//
+// promise
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
 
-
-let person = {
-  name: 'Jack',
-  age: 20,
-  [Symbol.for('password')]: 'Jack20',
-  [Symbol.for('nickname')]: 'Freeman'
-}
-
-// console.log(person[Symbol.for('password')]);
-console.log(Object.getOwnPropertySymbols(person));
+fetch('https://jsonplaceholder.typicode.com/posts/')
+  .then(res => res.json())
+  .then(data => data)
+  .then(data => console.log('!!!', data))
+  //.then(data => render(data))
+  .catch(() => console.log('some error...'));
