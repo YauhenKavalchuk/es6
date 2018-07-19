88c78b43c2768704bb6ce79ca2a41dd6e6d31dc4
Yauhen <silent_control@tut.by>
2018-07-19 15:34:00 +0300

    Lesson 17 (Set & Map)

--- app/js/common.js
+++ app/js/common.js
@@ -1,40 +1,57 @@
-// Generators
+// Set & Map
 // ---------------
-/*
-function *generate() {
-  try {
-    yield 1;
-    yield 2;
-    yield 3;
-  } catch (err) {
-    console.log('Custom error: ', err);
-  }
-}
-let iterator = generate();
-console.log(iterator.next());
-console.log(iterator.throw(new Error('some error...')));
-console.log(iterator.next());
-*/
-/*
-function *generateRange(start, end) {
-  let current = start;
-  while(current <= end) {
-    yield current++;
-  }
-}
 
-for (let number of generateRange(1, 10)) {
-  console.log(number);
-}
+// Map
+/*
+let map = new Map();
+
+map
+  .set('str', 'string')
+  .set(1, 'number')
+  .set(true, 'boolean');
+
+console.log(map.get(1));
+console.log(map.get(true));
+console.log(map.size);
 */
-let object = {
-  *generateRange(start, end) {
-    let current = start;
-    while(current <= end) {
-      yield current++;
-    }
-  }
-}
-for (let number of object.generateRange(1, 10)) {
-  console.log(number);
-}
+
+// let map = new Map([
+//   ['str', 'string'],
+//   [1, 'number'],
+//   [true, 'boolean']
+// ])
+
+// console.log(map.get(1));
+// console.log(map.get(true));
+// console.log(map.size);
+// console.log(map.has(1));
+// console.log(map.delete(1));
+// console.log(map.size);
+// console.log(map.clear());
+// console.log(map.size);
+// Iteration
+// keys(), values(), entries()
+
+
+// Set
+let jack = {name: 'Jack'};
+let max = {name: 'Max'};
+let leo = {name: 'Leo'};
+
+let users = new Set();
+
+users
+  .add(jack)
+  .add(max)
+  .add(leo)
+  .add(jack)
+  .add(max)
+console.log(users.size);
+users.forEach(user =>console.log(user));
+//Methods
+//add()
+//delete(item)
+//has(item)
+//clear()
+
+// WeackMap & WeackSet
